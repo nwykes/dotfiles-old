685ba5bf33073b2308d18ac1e4016c3e6604e728
nwykes <nwykes@gmail.com>
2015-02-23 20:44:39 -0600

    bootstrap.fish work in progress

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,7 +9,7 @@ set fish_theme agnoster
 # All built-in plugins can be found at ~/.oh-my-fish/plugins/
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Enable plugins by adding their name separated by a space to the line below.
-set fish_plugins theme
+set fish_plugins theme vundle
 
 # Path to your custom folder (default path is ~/.oh-my-fish/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
