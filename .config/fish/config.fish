779d6ad0d7024c4797f32f922b636befef4ad5b3
nwykes <nwykes@gmail.com>
2015-06-23 16:39:05 -0600

    fix oh-my-fish custom path and add vundle back in

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ set -x PAGER vimpager
 
 # Path to your custom folder (default path is ~/.oh-my-fish/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
+set fish_custom $HOME/.config/fish/custom
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
@@ -16,3 +17,4 @@ set -x PAGER vimpager
 # Plugins and themes can be found at https://github.com/oh-my-fish/
 Theme 'agnoster'
 Plugin 'theme'
+Plugin 'vundle'
