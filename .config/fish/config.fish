4a198f6a86704657e71ae03f78ee0f7680d154ca
nwykes <nwykes@gmail.com>
2015-06-23 16:33:30 -0600

    fix oh-my-fish submodule url.  update submodules.  fix config.fish for new version

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,21 +6,13 @@ eval sh $HOME/.colors/base16-shell/base16-default.dark.sh
 
 set -x PAGER vimpager
 
-# Theme
-set fish_theme agnoster
-
-# All built-in plugins can be found at ~/.oh-my-fish/plugins/
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Enable plugins by adding their name separated by a space to the line below.
-set fish_plugins theme vundle
-
 # Path to your custom folder (default path is ~/.oh-my-fish/custom)
-set fish_custom $HOME/.config/fish/custom
+#set fish_custom $HOME/dotfiles/oh-my-fish
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
 
-Theme "agnoster"
-
-Plugin "theme"
-Plugin "vundle"
+# Custom plugins and themes may be added to ~/.oh-my-fish/custom
+# Plugins and themes can be found at https://github.com/oh-my-fish/
+Theme 'agnoster'
+Plugin 'theme'
