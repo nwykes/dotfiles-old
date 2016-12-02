9f2555ef1e02632147da8f27664511e9f40334fb
nwykes <nwykes@gmail.com>
2016-12-01 23:14:40 -0700

    ls colorization setup for different OSes. Show user when on ssh

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -1,10 +1,22 @@
-# Base16 Shell
-# eval sh $HOME/.colors/base16-shell/scripts/base16-eighties.sh
+
 source ~/.config/omf/git_alias.fish
 
 set -g GOPATH ~/go
 
 set -g theme_color_scheme gruvbox
 
-set -gx CLICOLOR 1
-set -gx CLICOLOR_FORCE 1
+switch (uname)
+    case Linux
+            function ls
+                 set -l executable ls --color=always
+                 grc.wrap $executable $argv
+            end
+    case Darwin
+        set -gx CLICOLOR 1
+        set -gx CLICOLOR_FORCE 1
+end
+
+if begin; test -n "$SSH_CLIENT"; or test -n "$SSH_TTY"; end
+        set -g theme_display_user yes
+end
+
