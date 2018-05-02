fe8845cc504ad3b15ffd590294e2979c4caff1cd
nwykes <nwykes@gmail.com>
2018-05-01 22:23:44 -0600

    use powerline-go

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,17 @@
 function fish_prompt
-    powerline-shell --shell bare $status
+    ~/go/bin/powerline-go -theme ~/.config/powerline-go/default.json -error $status -modules "venv,cwd,perms,git,exit,root" -shell bare
 end
 
 source /usr/local/etc/grc.fish
 source ~/.config/fish/git_alias.fish
 
+switch (uname)
+    case Linux
+            function ls
+                 set -l executable ls --color=always
+                 grc.wrap $executable $argv
+            end
+    case Darwin
+        set -gx CLICOLOR 1
+        set -gx CLICOLOR_FORCE 1
+end
